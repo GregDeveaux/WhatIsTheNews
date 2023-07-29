b468e2d7c31d1c2a2e25e626afb2555528a2a79c
GregDeveaux <92979399+GregDeveaux@users.noreply.github.com>
2023-07-29 10:43:38 +0200

    init project What is the news

---
+++ WhatIsTheNews/WhatIsTheNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsTheNewsApp.swift
+//  WhatIsTheNews
+//
+//  Created by Greg-Mini on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsTheNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
